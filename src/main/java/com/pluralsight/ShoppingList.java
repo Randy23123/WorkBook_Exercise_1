72880b04e57b150a0d865ef6f80dde47fdfa725c
Randy23123 <rlopez@appdev.yearup.org>
2023-10-04 13:43:47 -0400

    git commit push

--- src/main/java/com/pluralsight/ShoppingList.java
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -1,7 +1,11 @@
-public class ShoppingLgit ist {
+package com.pluralsight;
 
+public class ShoppingList {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        String[] shoppingList = {"egg", "milk", "cereal", "rice", "beans", "soda", "chips","beer", "platains","yogurt"};
 
+        for (int i = 0; i < shoppingList.length; i++) {
+            System.out.println(shoppingList[i]);
+        }
+    }
 }
